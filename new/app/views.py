a24a2009153596fd45a3d8baf1a509b9ecb86baf
Durbek <pattoyevd@gmail.com>
2022-12-13 22:12:19 +0500

    views qoshish

---
+++ new/app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import *
+
+def home(request):
+    categories = request.GET.get("category")
+    if categories == None:
+        post = Post.objects.all()
+    else:
+        post = Post.objects.filter(category__category_name=categories)
+        
+    context = {
+        "category":Category.objects.all(),
+        "post":post
+    }
+    return render(request, 'index.html', context)
